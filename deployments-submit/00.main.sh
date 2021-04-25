0426d1a6d7ea4ff853ba58cdee47def51019823d
Jon Grey <jon.arnold.grey@gmail.com>
2021-04-25 19:29:18 +0200

    Lazy update at Sun Apr 25 19:29:18 CEST 2021.

--- deployments-submit/00.main.sh
+++ deployments-submit/00.main.sh
@@ -5,8 +5,8 @@ echo "
 ###########################################################################
 #### Test infrastructure management deployments setup
 ###########################################################################"
-bash 03.test.local.terraform.sh
+bash 03.test.local.docker-compose.sh
 
 git add --all
 git commit -m "Lazy update at $(date)."
-git push --set-upstream origin branch
+git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)
